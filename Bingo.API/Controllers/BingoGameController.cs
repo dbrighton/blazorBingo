fb32fc5df4119e0816ff3440c3ad7337c19a2c7a
David Brighton <DaveBrighton@msn.com>
2021-09-12 08:11:52 -0400

    Added start game method

---
+++ Bingo.API/Controllers/BingoGameController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Bingo.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BingoGameController : ControllerBase
+    {
+        private readonly ILogger<BingoGameController> _logger;
+
+        public BingoGameController(ILogger<BingoGameController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("start")]
+        public void StartNewGame()
+        {
+            _logger.LogInformation($"In StartNewGame");
+        }
+    }
+}
